19c3ef19e63df48aa2818011a3b8a8bff3e22d8f
fabianmeneses56 <fabianmeneses56@hotmail.com>
2024-06-18 22:10:59 -0500

    fix style

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -84,7 +84,7 @@ struct ContentView: View {
                 .frame(maxWidth: .infinity)
                 .padding(.vertical, 20)
                 .background(.regularMaterial)
-//                .clipShape(.rect(cornerRadius: 20))
+                .clipShape(.rect(cornerRadius: 20))
                 
                 Spacer()
                 Spacer()
